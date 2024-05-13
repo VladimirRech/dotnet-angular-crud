01d0ec967ad37ee10a93d780f17edf3582515e74
Vladimir Rech <rechvladimir@hotmail.com>
2024-05-13 20:21:06 -0300

    Classe renomeada

--- src/expenses.domain/entities/BaseClass.cs
+++ src/expenses.domain/entities/BaseClass.cs
@@ -1,8 +1,8 @@
 using expenses.domain.interfaces;
 
-namespace expenses.domain.classes;
+namespace expenses.domain.entities;
 
-public class Base : IBase
+public class BaseClass : IBase
 {
         public int Id { get; set; }
         public DateTime UpdateDate { get; set; }
